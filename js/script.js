af6b0953c868623100b99a999b5698f39d89914f
AZLANSYAH123 <azlansyah810@gmail.com>
2023-09-28 23:13:13 -0700

    new

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
+  $(".show-popup-Infak").click(function(){
+    $(".wrapper-popup").addClass('show');
+   
+  });
+  $(".close-popup").click(function(){
+  $(".wrapper-popup").removeClass('show');
+  });
     $('.hamburger').click(function(){
         $(".top-menu").css("right","0");
     });
@@ -20,4 +28,28 @@ $(document).ready(function(){
         prevEl: ".swiper-button-prev",
       },
     });
-});
+   
+    var swiper = new Swiper(".Swiper-kegiatan", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      },
+    });
+  });
